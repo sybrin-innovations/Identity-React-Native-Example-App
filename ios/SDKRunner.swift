8e8d01a8b8248b974b5821a81ffab741e9900150
Rhulani Ndhlovu <rhulani.ndhlovu@sybrin.com>
2024-05-20 01:30:18 +0200

    updates

--- ios/SDKRunner.swift
+++ ios/SDKRunner.swift
@@ -17,13 +17,10 @@ class SDKRunner: NSObject {
     
   }
   
-  let licenseKey: String = "R+hTAuG54bKFpUXInQmkzv7Ehj84knBdOPTmCsIk14zWXnR/ADOnLtAM+Jt/qKWZsbi1932SZz5HChZwV3UBzhQRElHPQJlUGw94LFY2OvjsvgqyLbU1OBcPhZk7c5VrXQnOOhnE/0clGDF4crWIf76kcoLO8db1mQ7XcVlMT78hox9AH689DpSObkEvBgU4+0Ec6pTOPdBLvscOktir0tkKH8PHskFyqwOGSaBWVRYMSRK+KkKDPLB2urK/0VOLItU7nXuY85z3eA+QS90jMqaXKGz3TNcuTB1gqf2sD9Rl9W81z56u2o0IU4Vs6vFNXdTfOeSnRjYW8BDg3WkHYzlSahT2io8WGi3alXkbsDW+R4i3i5fiJ1CCB/SvWo5tYiEAgQTD08FbSH5vWcR+3OIhJUGlNgnMs+L6euWc9GYT4OhKXH6jAPPLweSCFH2KhntKnYxRQl2PISOiymWCZZBLe8iLm15gwkBHsCiPZ/ie1S+BDqLbD4bxuxgfw6A/vnlj3AU5Nex7BbyCVaqqsQ=="
+  let licenseKey: String = ""
   
   @objc
   func scan(_ callback: @escaping RCTResponseSenderBlock) {
-    //callback(["it works"])
-    
-    print("hello");
     
     DispatchQueue.main.async {
       let rooViewController = UIApplication.shared.delegate?.window??.rootViewController
@@ -32,21 +29,21 @@ class SDKRunner: NSObject {
         return
       }
       
-      //self.runIdentitySDK(rooViewController, callback)
+      self.runIdentitySDK(rooViewController, callback)
     }
   }
   
   func runIdentitySDK(_ viewController: UIViewController, _ callback: RCTResponseSenderBlock) {
     
-//    let sybrinConfig = SybrinIdentityConfiguration(license: licenseKey)
-//    sybrinConfig.language = .ENGLISH
-//    SybrinIdentity.shared.configuration = sybrinConfig
-//
-//    SybrinIdentity.shared.scanPassport(on: viewController, for: .Philippines) {
-//      (result, message) in
-//
-//      print("done launching")
-//    }
+   let sybrinConfig = SybrinIdentityConfiguration(license: licenseKey)
+   sybrinConfig.language = .ENGLISH
+   SybrinIdentity.shared.configuration = sybrinConfig
+
+   SybrinIdentity.shared.scanPassport(on: viewController, for: .Philippines) {
+     (result, message) in
+
+     print("done launching")
+   }
   }
   
   @objc static func requiresMainQueueSetup() -> Bool { return true }
